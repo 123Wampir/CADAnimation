26bcd766f3199a9e29ed5cc32ee10604d91166ee
123Wampir <dimaworonin73@gmail.com>
2023-09-14 21:49:51 +0300

    Added vercel/analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,6 @@ import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
 import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import { Reflector } from './shared/Reflector/Reflector';
 import { SceneManagerService } from './services/scene.manager/scene.manager.service';
-import { inject } from '@vercel/analytics';
-inject();
 
 
 @Component({
